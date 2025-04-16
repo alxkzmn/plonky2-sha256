43bb51b27e89c7f98e9c425a1fc26ebfb8abad1d
Alex Kuzmin <alex.kuzmin@proton.me>
2025-04-16 14:13:15 +0800

    Add github workflow

--- src/bin/measure.rs
+++ src/bin/measure.rs
@@ -1,30 +1,43 @@
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
 
-use jemalloc_ctl::{
-    epoch,
-    stats::{self},
-};
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
+use memory_stats::memory_stats;
 use plonky2::{plonk::config::PoseidonGoldilocksConfig, util::serialization::Write};
 use plonky2_sha256::bench::{prove, sha256_no_lookup_prepare};
 use plonky2_u32::gates::arithmetic_u32::{U32GateSerializer, U32GeneratorSerializer};
+use tikv_jemalloc_ctl::raw;
+use tikv_jemalloc_ctl::{
+    epoch,
+    stats::{self},
+};
 
 const D: usize = 2;
 type C = PoseidonGoldilocksConfig;
 
 fn main() {
     epoch::advance().unwrap();
-    //let allocated_before = stats::allocated::read().unwrap();
+    let allocated_before = stats::allocated::read().unwrap();
     let resident_before = stats::resident::read().unwrap();
+    let usage_before = memory_stats().unwrap();
 
     let (data, pw) = sha256_no_lookup_prepare();
 
+    let usage_after = memory_stats().unwrap();
+    println!(
+        "memory_stats: Preprocessing: {} GB resident | {} GB virt",
+        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
+        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+    );
     epoch::advance().unwrap();
-    //let allocated_after = stats::allocated::read().unwrap();
+    let allocated_after = stats::allocated::read().unwrap();
     let resident_after = stats::resident::read().unwrap();
     println!(
-        "Preprocessing: resident memory: {}GB",
-        //(allocated_after - allocated_before) as f32 / 1024.0 / 1024.0 / 1024.0,
+        "jemalloc: Preprocessing: {} GB alloc | {} GB resident",
+        (allocated_after - allocated_before) as f32 / 1024.0 / 1024.0 / 1024.0,
         (resident_after - resident_before) as f32 / 1024.0 / 1024.0 / 1024.0,
     );
 
@@ -41,22 +54,32 @@ fn main() {
         "Common data size: {}B, Prover data size: {}B",
         common_data_size, prover_data_size
     );
+    let _ = unsafe { raw::write(b"stats.print\0", &true) };
     epoch::advance().unwrap();
-    //let allocated_before = stats::allocated::read().unwrap();
+    let allocated_before = stats::allocated::read().unwrap();
     let resident_before = stats::resident::read().unwrap();
 
+    let usage_before = memory_stats().unwrap();
+
     let proof = prove(&data.prover_data(), pw);
+
+    let usage_after = memory_stats().unwrap();
+    println!(
+        "memory_stats: Proving: {} GB resident | {} GB virt",
+        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
+        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+    );
+
     epoch::advance().unwrap();
-    //let allocated_after = stats::allocated::read().unwrap();
+    let allocated_after = stats::allocated::read().unwrap();
     let resident_after = stats::resident::read().unwrap();
     println!(
-        "Proving: resident memory: {}GB",
-        // Allocation numbers for prover don't make sense (~17179870000GB?)
-        //(allocated_after - allocated_before) as f32 / 1024.0 / 1024.0 / 1024.0,
+        "Proving: {} GB alloc | {} GB resident",
+        (allocated_after - allocated_before) as f32 / 1024.0 / 1024.0 / 1024.0,
         (resident_after - resident_before) as f32 / 1024.0 / 1024.0 / 1024.0,
     );
 
     let mut buffer = Vec::new();
     buffer.write_proof(&proof.proof).unwrap();
-    println!("Proof size: {}B", buffer.len());
+    println!("Proof size: {} KB", buffer.len() as f32 / 1024.0);
 }
